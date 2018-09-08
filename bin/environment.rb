fd313c4687e93c1e958491dbf79f983852de2b5a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-08 23:01:50 +0000

    automatically backed up by learn

--- bin/environment.rb
+++ bin/environment.rb
@@ -9,7 +9,8 @@ require_relative "../lib/scraper"
 require_relative "sql_runner"
 
 @db = SQLite3::Database.new('../pokemon.db')
-@db.execute("DROP TABLE IF EXISTS pokemon;")
 #connects ruby to our database.
+@db.execute("DROP TABLE IF EXISTS pokemon;")
+
 @sql_runner = SQLRunner.new(@db)
 @sql_runner.execute_schema_migration_sql
